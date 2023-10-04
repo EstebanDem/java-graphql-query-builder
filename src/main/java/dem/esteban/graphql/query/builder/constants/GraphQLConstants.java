2545c33aa76eb763f39d217d6a4b25fc3f5ff05d
Esteban Demarchi <estebanjosedemarchi@gmail.com>
2023-10-04 18:12:04 -0300

    Change to a non-instantiable class to hold constants instead of an interface

--- src/main/java/dem/esteban/graphql/query/builder/constants/GraphQLConstants.java
+++ src/main/java/dem/esteban/graphql/query/builder/constants/GraphQLConstants.java
@@ -1,27 +1,30 @@
 package dem.esteban.graphql.query.builder.constants;
 
-public interface GraphQLConstants {
-    String QUERY_WITH_PARAMETERS_FORMAT ="query Query(%s) { %s }";
+public final class GraphQLConstants {
+    private GraphQLConstants() {
+    }
+
+    public static String QUERY_WITH_PARAMETERS_FORMAT ="query Query(%s) { %s }";
 
     /**
      * String format of non-mandatory variable
      * For example: $varName: ID
      */
-    String QUERY_VARIABLE_TEMPLATE = "$%s: %s";
+    public static String QUERY_VARIABLE_TEMPLATE = "$%s: %s";
 
     /**
      * String format of mandatory variable
      * For example: $varName: ID!
      */
-    String QUERY_MANDATORY_VARIABLE_TEMPLATE = "$%s: %s!";
+    public static String QUERY_MANDATORY_VARIABLE_TEMPLATE = "$%s: %s!";
 
-    String QUERY_PARAM_VARIABLES_SEPARATOR = ", ";
+    public static String QUERY_PARAM_VARIABLES_SEPARATOR = ", ";
 
     // Types
-    String TYPE_STRING = "String";
-    String TYPE_INT = "Int";
-    String TYPE_FLOAT = "Float";
-    String TYPE_BOOLEAN = "Boolean";
-    String TYPE_ID = "ID";
-    String TYPE_MANDATORY = "!";
+    public static String TYPE_STRING = "String";
+    public static String TYPE_INT = "Int";
+    public static String TYPE_FLOAT = "Float";
+    public static String TYPE_BOOLEAN = "Boolean";
+    public static String TYPE_ID = "ID";
+    public static String TYPE_MANDATORY = "!";
 }
