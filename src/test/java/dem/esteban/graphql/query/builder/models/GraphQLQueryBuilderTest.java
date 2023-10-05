04690f493f0c2ff9fb85235a251f2039f7028243
Esteban Demarchi <estebanjosedemarchi@gmail.com>
2023-10-05 18:35:24 -0300

    Update class name

--- src/test/java/dem/esteban/graphql/query/builder/models/GraphQLQueryBuilderTest.java
+++ src/test/java/dem/esteban/graphql/query/builder/models/GraphQLQueryBuilderTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class GraphQLQueryTest {
+class GraphQLQueryBuilderTest {
 
     @Test
     public void testQueryWithOneVariable() {
